62553b1ea4292167cc08cd3e24ef45d261d7204e
Marcus Butler <marcus@pop.local.asterick.co>
2020-09-13 16:26:48 -0500

    add pcost function and handle missing fields

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,14 +2,24 @@
 #
 # Exercise 1.27
 
-sum = 0
-with open("Work/Data/portfolio.csv", mode="r") as f:
 
-    headers = next(f).split(",")
-    for line in f:
-        line = line.strip()
-        line = line.replace('"', "")
-        line = line.split(",")
-        sum += int(line[1]) * float(line[2])
+def portfolio_cost(filename):
 
-print(f"Total cost {sum}")
+    sum = 0
+    with open(filename, mode="r") as f:
+        next(f)
+        for line in f:
+            line = line.strip()
+            line = line.replace('"', "")
+            line = line.split(",")
+            try:
+                sum += int(line[1]) * float(line[2])
+            except ValueError:
+                print(f"Found missing fields - {line}")
+                continue
+    return sum
+
+
+cost = portfolio_cost("Work/Data/missing.csv")
+
+print(f"Total cost {cost}")
